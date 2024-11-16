f0bf59e0033bbf7e5f5843606fcfd1e8cdf6ee58
Daftones <daffayudhamusyafa22@gmail.com>
2024-11-16 12:05:00 +0700

    nav pop push

--- wisata_candi/lib/widgets/item_card.dart
+++ wisata_candi/lib/widgets/item_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wisata_candi/detail_screen.dart';
 import 'package:wisata_candi/models/candi.dart';
 
 class ItemCard extends StatelessWidget {
@@ -8,43 +9,54 @@ class ItemCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //todo 6 : Implementasi routing ke detailscreen
+
     //todo 2 :.....
-    return Card(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-      margin: EdgeInsets.all(4),
-      elevation: 1,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          //todo 3 : Image widget
-          Expanded(
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(15),
-              child: Image.asset(
-                candi.imageAsset,
-                width: double.infinity,
-                fit: BoxFit.cover,
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => DetailScreen(candi: candi),
+          ),
+        );
+      },
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        margin: EdgeInsets.all(4),
+        elevation: 1,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //todo 3 : Image widget
+            Expanded(
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(15),
+                child: Image.asset(
+                  candi.imageAsset,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: 16, top: 8),
-            child: Text(
-              candi.name,
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            //todo 4 : Text
+            Padding(
+              padding: EdgeInsets.only(left: 16, top: 8),
+              child: Text(
+                candi.name,
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              //todo 5 : text
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: 16, bottom: 8),
-            child: Text(
-              candi.type,
-              style: TextStyle(fontSize: 16),
+            Padding(
+              padding: EdgeInsets.only(left: 16, bottom: 8),
+              child: Text(
+                candi.type,
+                style: TextStyle(fontSize: 16),
+              ),
             ),
-          ),
-
-          //todo 4 : Text
-          //todo 5 : text
-        ],
+          ],
+        ),
       ),
     );
   }
